d2d4809e0562883e49a81381c33c21bc98a55384
RobertPoghosyan <[robertpoghosian@gmail.com]>
2020-09-20 10:03:58 +0400

    ToDo List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Person from './components/Person';
 import Product from './components/Product';
-//import Description from './components/Description';
-//import Price from './components/Price';
-
+import Lesson6 from './lesson6/Lesson6'
+import ToDo from './toDoList/ToDo';
 
 function App() {
   return (
@@ -11,9 +10,8 @@ function App() {
       <header className="App-header">
         <Person name ="Robert" project="Great Project"/>
         <Product fruits ="Apricot" price = "2$" description="The best apricots from Armenia" />
-        
-        
-        
+        <Lesson6 />
+        <ToDo />
         
       </header>
     </div>
