c2f5fdacfb0a2191bbcc6f2da72283eaa88fb9eb
RobertPoghosyan <[robertpoghosian@gmail.com]>
2020-09-26 13:49:34 +0400

    merge finally

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-<<<<<<< HEAD
-import ToDo from './Components/ToDo';
-=======
 import Person from './components/Person';
-import Product from './components/Product';
+import Product fr"om './components/Product';
 import Lesson6 from './lesson6/Lesson6'
 
->>>>>>> master
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        
-        <ToDo />
-=======
         <Person name ="Robert" project="Great Project"/>
         <Product fruits ="Apricot" price = "2$" description="The best apricots from Armenia" />
         <Lesson6 />
         
->>>>>>> master
         
       </header>
     </div>
